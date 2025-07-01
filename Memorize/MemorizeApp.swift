2858bcfdeca0d029e78f61abc4d8386a49c96ab8
Celio Junior <cxliobj@outlook.com>
2025-07-01 16:23:26 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Celio Junior on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
