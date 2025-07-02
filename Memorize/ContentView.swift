e1076dbbbfa288ddba3c04eb835781b634f6a37f
Celio Junior <cxliobj@outlook.com>
2025-07-02 13:26:39 -0300

    Foggleable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
+            ForEach(0..<4) { _ in
+                CardView()
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -21,19 +20,26 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
+    
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 6)
                 Text("👻").font(.largeTitle)
             }
             else {
-                RoundedRectangle(cornerRadius: 20)
+                base.fill()
             }
         }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
+}
+
+#Preview {
+    ContentView()
 }
