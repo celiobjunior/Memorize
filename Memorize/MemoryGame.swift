b9f41c8fc615996d5e65a257ab10a5f9600b5963
Celio Junior <cxliobj@outlook.com>
2025-07-14 13:50:09 -0400

    gridItemWidthThatFits and GeometryReader

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -10,7 +10,7 @@ import Foundation
 struct MemoryGame<CardContent> where CardContent: Equatable {
     
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
-        var isFaceUp = false
+        var isFaceUp = true
         var isMatched = false
         let content: CardContent
         
